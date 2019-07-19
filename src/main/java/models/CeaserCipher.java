c62744f8651d959a7e74798726f8f1176e42b267
Bannette <banetaloo3@gmail.com>
2019-07-19 11:28:00 +0300

    created a test to check wether the constructor instantiates properly

---
+++ src/main/java/models/CeaserCipher.java
@@ -0,0 +1,12 @@
+package models;
+
+public class CeaserCipher {
+
+    private String mWord;
+    private int mKey;
+
+    public CeaserCipher(String word, int key) {
+        this.mWord = word;
+        this.mKey = key;
+    }
+}
--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,7 +1,19 @@
+package models;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CeaserCipherTest {
-    
+    @Test
+    public void ceaserCipher_instantiatesCorrectly_true() {
+        CeaserCipher testCeaser = new CeaserCipher("Hello", 2);
+        assertTrue(testCeaser instanceof CeaserCipher);
+
+    }
+    @Test
+    public void ceaserCipher_getmKey_int() {
+        CeaserCipher testCeaser = new CeaserCipher("Hello",2);
+        assertEquals(2,testCeaser.getmKey());
+    }
 }
